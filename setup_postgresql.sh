1d8c5a24654af68052b338223f1ace4759df51a6
Yanisse YOUMI <yanisse.youmi@ynov.com>
2019-06-04 11:37:33 +0200

    add setup bash

---
+++ setup_postgresql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# FIRST STEP : on crée la liste pgdg
+# sudo touch /etc/apt/sources.list.d/pgdg.list
+
+# On y ajoute une ligne de repo dans ce même fichier
+echo deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main > /etc/apt/sources.list.d/pgdg.list
+# SECOND STEP : On importe le repo avec la clé
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# THIRD STEP : On actualise en mettant a jour la debian
+apt-get update
+# NEXT STEP : On installe enfin postgresql
+apt-get install postegresql-11
+echo "Script terminé fin d'execution"
