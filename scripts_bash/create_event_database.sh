b92302ce2e4c867c9e47d7e8f3906efe7555317b
Yanisse YOUMI <yanisse.youmi@ynov.com>
2019-06-05 09:51:31 +0200

    maj dir

--- scripts_bash/create_event_database.sh
+++ scripts_bash/create_event_database.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# Déclaration des nouveaux répertoires
+FILE_DIRECTORY=`dirname $0`
+PROJECT_DIRECTORY=$FILE_DIRECTORY/..
+
+# Création des tables dans la database via le script créé
--- scripts_bash/import_github.sh
+++ scripts_bash/import_github.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Déclaration des nouveaux répertoires
+FILE_DIRECTORY=`dirname $0`
+PROJECT_DIRECTORY=$FILE_DIRECTORY/..
+
+# Si l'archive existe déjà on la supprime
+rm -Rf $PROJECT_DIRECTORY/2019-05-15-20.json.gz
+
 # Déclaration d'une variable URL qui récupère l'archive de données json
 URL=https://data.gharchive.org/2019-05-15-20.json.gz
 # On récupère celle ci ensuite pour la télécharger
