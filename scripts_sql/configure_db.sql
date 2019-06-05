7b9ce15961457409a71abd7bd7f987e6a8837924
Yanisse YOUMI <yanisse.youmi@ynov.com>
2019-06-05 10:30:33 +0200

    modif db sql

--- scripts_sql/configure_db.sql
+++ scripts_sql/configure_db.sql
@@ -1,2 +1,5 @@
 ALTER SYSTEM SET PORT = '1234';
 ALTER SYSTEM SET work_mem = '50MB';
+
+SELECT name, setting FROM pg_settings WHERE name='port';
+SELECT name, setting FROM pg_settings WHERE name='work_mem';
