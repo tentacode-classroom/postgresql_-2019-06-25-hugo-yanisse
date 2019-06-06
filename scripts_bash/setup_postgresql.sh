8f5f1eb7be0956647dd166101f6acf15056d1a36
hugo <hugola1@hotmail.fr>
2019-06-06 12:40:50 +0200

    add event_raw script

--- scripts_bash/setup_postgresql.sh
+++ scripts_bash/setup_postgresql.sh
@@ -4,13 +4,14 @@
 # sudo touch /etc/apt/sources.list.d/pgdg.list
 
 # On y ajoute une ligne de repo dans ce même fichier
-echo deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main > /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main
+" | sudo tee --append /etc/apt/sources.list.d/pgdg.list
 # SECOND STEP : On importe le repo avec la clé
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 # THIRD STEP : On actualise en mettant a jour la debian
-apt-get update
+sudo apt-get update
 # NEXT STEP : On installe enfin postgresql
-apt-get install postegresql-11 -y
+sudo apt-get install postgresql-11 -y
 
 # Message affichant la fin d'execution du script
 echo "Script terminé fin d'execution"
