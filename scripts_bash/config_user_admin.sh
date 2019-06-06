aae3177f3eb8cc75a5dcaf419b8985936fe15659
Yanisse YOUMI <yanisse.youmi@ynov.com>
2019-06-06 10:28:12 +0200

    correct script sql

--- scripts_bash/config_user_admin.sh
+++ scripts_bash/config_user_admin.sh
@@ -5,6 +5,4 @@ PROJECT_DIRECTORY=$FILE_DIRECTORY/..
 # Lancement du script sql "configure_db"
 sudo -u postgres psql -f $PROJECT_DIRECTORY/scripts_sql/create_user_admin.sql
 # Vérification avec connexion du nouvel user github_events créé
-sudo psql --username github_events -c "SELECT 'Je suis connecté sur github.';"
-# Redémarrage du service postresql afin de prendre en compte les modifications apportées
-sudo service postgresql restart
+psql --username=github_events -c "SELECT 'Je suis connecté sur github.';"
